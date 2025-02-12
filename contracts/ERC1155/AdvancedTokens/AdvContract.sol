bd7212db0d2ce51b93771c8041d1c79c17c6578d
unknown <beguareyahya01@gmail.com>
2025-02-12 23:59:17 +0100

    feat: The process automation to upload tokens Art and metadata to IPFS

--- contracts/ERC1155/AdvancedTokens/AdvContract.sol
+++ contracts/ERC1155/AdvancedTokens/AdvContract.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.21;
+pragma solidity ^0.8.20;
 
 import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
